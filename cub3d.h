b4e27988e652761327a9e3f171b1b1c81cc1a188
irene <wunder.frau@yandex.by>
2024-09-15 18:55:55 +0300

    feature: create firs version of read function logic

---
+++ cub3d.h
@@ -0,0 +1,12 @@
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "libft/libft.h"
+# include "MLX42/include/MLX42/MLX42.h"
+# include "vector.h"
+# include "fcntl.h"
+# include <stdio.h>
+
+t_vector	*read_map(char **argv);
+
+#endif
