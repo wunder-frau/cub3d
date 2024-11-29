432878144656bf81f079f4745c3f95b12df69586
irene <wunder.frau@yandex.by>
2024-11-29 14:28:11 +0200

    test

--- src/cleanup.c
+++ src/cleanup.c
@@ -6,54 +6,72 @@
 /*   By: istasheu <istasheu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 14:49:37 by istasheu          #+#    #+#             */
-/*   Updated: 2024/11/24 19:55:18 by istasheu         ###   ########.fr       */
+/*   Updated: 2024/11/29 14:26:27 by istasheu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-static void	cleanup_textures_and_images(t_game *game)
+static void cleanup_textures_and_images(t_game *game)
 {
-	if (!game)
-		return ;
-	if (game->assets.textures.no)
-		mlx_delete_texture(game->assets.textures.no);
-	if (game->assets.textures.so)
-		mlx_delete_texture(game->assets.textures.so);
-	if (game->assets.textures.ea)
-		mlx_delete_texture(game->assets.textures.ea);
-	if (game->assets.textures.we)
-		mlx_delete_texture(game->assets.textures.we);
-	if (game->image)
-		mlx_delete_image(game->mlx, game->image);
+    if (!game)
+        return;
+
+    if (game->assets.textures.no)
+    {
+        mlx_delete_texture(game->assets.textures.no);
+        game->assets.textures.no = NULL;
+    }
+    if (game->assets.textures.so)
+    {
+        mlx_delete_texture(game->assets.textures.so);
+        game->assets.textures.so = NULL;
+    }
+    if (game->assets.textures.ea)
+    {
+        mlx_delete_texture(game->assets.textures.ea);
+        game->assets.textures.ea = NULL;
+    }
+    if (game->assets.textures.we)
+    {
+        mlx_delete_texture(game->assets.textures.we);
+        game->assets.textures.we = NULL;
+    }
+
+    if (game->image)
+    {
+        mlx_delete_image(game->mlx, game->image);
+        game->image = NULL;
+    }
 }
 
-static void	cleanup_map(t_game *game)
+static void cleanup_map(t_game *game)
 {
-	size_t	y;
+    size_t y;
 
-	if (!game || !game->map_grid)
-		return ;
-	if (game->map_grid->symbols)
-		free_assets_struct(&game->assets);
-	if (game->map_grid->symbols)
-	{
-		y = 0;
-		while (y < game->map_grid->capacity)
-		{
-			if (game->map_grid->symbols[y])
-			{
-				free(game->map_grid->symbols[y]);
-				game->map_grid->symbols[y] = NULL;
-			}
-			y++;
-		}
-		free(game->map_grid->symbols);
-		game->map_grid->symbols = NULL;
-	}
-	free(game->map_grid);
-	game->map_grid = NULL;
+    if (!game || !game->map_grid)
+        return;
+
+    if (game->map_grid->symbols)
+    {
+        y = 0;
+        while (y < game->map_grid->capacity)
+        {
+            if (game->map_grid->symbols[y])
+            {
+                free(game->map_grid->symbols[y]);
+                game->map_grid->symbols[y] = NULL;
+            }
+            y++;
+        }
+        free(game->map_grid->symbols);
+        game->map_grid->symbols = NULL;
+    }
+
+    free(game->map_grid);
+    game->map_grid = NULL;
 }
+
 
 static void	cleanup_game(t_game *game)
 {
@@ -66,6 +84,8 @@ static void	cleanup_game(t_game *game)
 
 void	cleanup(t_game *game)
 {
+	if (!game)
+	return ;
 	cleanup_textures_and_images(game);
 	cleanup_map(game);
 	cleanup_game(game);
--- src/free.c
+++ src/free.c
@@ -6,23 +6,41 @@
 /*   By: istasheu <istasheu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 10:05:09 by istasheu          #+#    #+#             */
-/*   Updated: 2024/11/24 19:45:37 by istasheu         ###   ########.fr       */
+/*   Updated: 2024/11/29 14:25:43 by istasheu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-void	free_assets_struct(t_assets *assets)
+void free_assets_struct(t_assets *assets)
 {
-	if (assets->textures.path_ea)
-		free(assets->textures.path_ea);
-	if (assets->textures.path_no)
-		free(assets->textures.path_no);
-	if (assets->textures.path_so)
-		free(assets->textures.path_so);
-	if (assets->textures.path_we)
-		free(assets->textures.path_we);
+    if (!assets)
+        return;
+
+    if (assets->textures.path_ea)
+    {
+        free(assets->textures.path_ea);
+        assets->textures.path_ea = NULL;
+    }
+    if (assets->textures.path_no)
+    {
+        free(assets->textures.path_no);
+        assets->textures.path_no = NULL;
+    }
+    if (assets->textures.path_so)
+    {
+        free(assets->textures.path_so);
+        assets->textures.path_so = NULL;
+    }
+    if (assets->textures.path_we)
+    {
+        free(assets->textures.path_we);
+        assets->textures.path_we = NULL;
+    }
+
+    free(assets);
 }
+
 
 void	free_split_rgb_array(char **rgb_arr)
 {
